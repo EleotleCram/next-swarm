28470c88ea9394daed8a9e8366ba35847c085e04
Marcel Toele <EleotleCram@gmail.com>
2025-03-10 23:29:12 +0100

    src/logging-websockets.js: added option --websocket-logging

--- src/logging-websockets.js
+++ src/logging-websockets.js
@@ -2,6 +2,8 @@ var sprintf=require("sprintf-js").sprintf;
 var _ = require('underscore-node');
 var bus = require("hermes-bus");
 
+var websocketLoggingEnabled = false;
+
 // Retain a local cache of what was logged to the dashboard and console,
 // so that, even if a web client connects later, it will still show all
 // relevant past messages.
@@ -12,20 +14,20 @@ var logAreas = {
 var supportedLogAreaNames = Object.keys(logAreas);
 
 bus.on("logMessage", function(loggerId, message){
-	if(_.contains(supportedLogAreaNames, loggerId)) {
+	if(websocketLoggingEnabled && _.contains(supportedLogAreaNames, loggerId)) {
 		logAreas[loggerId].push.apply(logAreas[loggerId], message.split(/\n/));
 	}
 });
 
 bus.on("logReset", function(loggerId){
-	if(_.contains(supportedLogAreaNames, loggerId)) {
+	if(websocketLoggingEnabled && _.contains(supportedLogAreaNames, loggerId)) {
 		logAreas[loggerId] = [];
 	}
 });
 
 // Adds a link to the web client reporting page
 bus.on("logReset", function(loggerId){
-	if(loggerId === "dashboard" && connectServerUri) {
+	if(websocketLoggingEnabled && loggerId === "dashboard" && connectServerUri) {
 		var outputTerminalWidth = (process.stdout.columns || 112);
 		bus.triggerLogMessage("dashboard", "\033[H" + sprintf("%"+outputTerminalWidth+"s", "Interactive log also available at: "+connectServerUri+"/report-task-status/"));
 	}
@@ -36,30 +38,46 @@ bus.on("connectServerStarted", function(connectServer) {
 	connectServerUri = connectServer.uri;
 });
 
-var WebSocketServer = require('ws').Server;
-
-var wss = new WebSocketServer({port: 3001});
-
-wss.on('connection', function(ws) {
-	var send = function(busMessageId, args) {
-		try {
-			ws.send(JSON.stringify({busMessageId: busMessageId, arguments: args}));
-		} catch(error) {
-			console.error(error);
-			// Workaround for unsubscribe:
-			send = function() {};
+bus.on("registerCommandlineArguments", function (parser) {
+	parser.addArgument(
+		['--websocket-logging'],
+		{
+			dest: 'websocketLoggingEnabled',
+			action: 'storeTrue',
+			help: 'Start an interactive logger accessible via a websocket server.'
 		}
-	};
+	);
+});
+bus.on("commandlineArgumentsParsed", function (args) {
+	if(args.websocketLoggingEnabled) {
+		var WebSocketServer = require('ws').Server;
 
-	["logMessage", "logReset"].forEach(function(busMessageId) {
-		bus.on(busMessageId, function() {
-			send(busMessageId, _.toArray(arguments));
-		});
-	});
+		var wss = new WebSocketServer({port: 3001});
 
-	supportedLogAreaNames.forEach(function(logAreaName) {
-		logAreas[logAreaName].forEach(function(logEntry) {
-			send("logMessage", [logAreaName, logEntry]);
+		wss.on('connection', function(ws) {
+			var send = function(busMessageId, args) {
+				try {
+					ws.send(JSON.stringify({busMessageId: busMessageId, arguments: args}));
+				} catch(error) {
+					console.error(error);
+					// Workaround for unsubscribe:
+					send = function() {};
+				}
+			};
+
+			["logMessage", "logReset"].forEach(function(busMessageId) {
+				bus.on(busMessageId, function() {
+					send(busMessageId, _.toArray(arguments));
+				});
+			});
+
+			supportedLogAreaNames.forEach(function(logAreaName) {
+				logAreas[logAreaName].forEach(function(logEntry) {
+					send("logMessage", [logAreaName, logEntry]);
+				});
+			});
 		});
-	});
+
+		websocketLoggingEnabled = args.websocketLoggingEnabled;
+	}
 });
